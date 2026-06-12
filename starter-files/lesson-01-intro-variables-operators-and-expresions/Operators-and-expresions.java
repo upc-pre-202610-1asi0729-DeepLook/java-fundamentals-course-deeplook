a4000564a43b0c3a4502312212bc6ef4259dc5c2
Alejandro Jave Chang <u202312510@upc.edu.pe>
2026-06-12 00:39:00 -0500

    Update Operators-and-expresions.java

--- starter-files/lesson-01-intro-variables-operators-and-expresions/Operators-and-expresions.java
+++ starter-files/lesson-01-intro-variables-operators-and-expresions/Operators-and-expresions.java
@@ -1,13 +1,15 @@
-public class Main {
+public class OperadoresYExpresiones {
     public static void main(String[] args) {
         int anioActual = 2026;
         int anioNacimiento = 2011;
         
-        // Expresión matemática usando el operador de resta
-        int edadCalculada = anioActual - anioNacimiento;
-
-        // Expresión de concatenación usando el operador de suma
-        System.out.println("Si naciste en " + anioNacimiento + " y estamos en " + anioActual + ", entonces cumples:");
-        System.out.println(edadCalculada + " años.");
+        // TODO: Declara una variable entera (int) llamada 'edadCalculada' 
+        // y usa el operador de resta (-) para calcular su valor utilizando las variables anteriores
+        
+        
+        // TODO: Usa el operador de suma (+) para unir texto y variables (concatenar).
+        // Construye y muestra en consola una frase explicativa utilizando 'edadCalculada', 'anioActual' y 'anioNacimiento'.
+        // Ejemplo de frase que debes lograr: "Si naciste en 2011 y estamos en 2026, tienes 15 años."
+        
     }
 }
