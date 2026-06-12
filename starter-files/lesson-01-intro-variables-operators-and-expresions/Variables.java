1581a9470aac3026d129f2de167698aa6458ff8b
Alejandro Jave Chang <u202312510@upc.edu.pe>
2026-06-12 00:25:11 -0500

    Update Variables.java

--- starter-files/lesson-01-intro-variables-operators-and-expresions/Variables.java
+++ starter-files/lesson-01-intro-variables-operators-and-expresions/Variables.java
@@ -1,1 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        String mensajeBienvenida = "Bienvenido al sistema de estudiantes.";
+        int edadEstudiante = 15;
 
+        System.out.println(mensajeBienvenida);
+        System.out.println("La edad del estudiante es: ");
+        System.out.println(edadEstudiante);
+    }
+}
